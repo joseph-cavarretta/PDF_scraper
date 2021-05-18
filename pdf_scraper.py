52cbd0f644fedc8316917d4545721438d7b1105f
Joseph Cavarretta <57957983+joseph-cavarretta@users.noreply.github.com>
2021-05-17 20:35:23 -0600

    Update pdf_scraper.py

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -1,20 +1,12 @@
 import pandas as pd
-import numpy as np
 import tabula
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-
-## Set Working Directory
-import os
-cwd = os.getcwd()
-
-## Set Working Directory
-wd = os.chdir(r'') ### Copy in the directory you saved the file to
 
 ## Input PDF file name
 file = "demo_costar_report.pdf"
 
 ## Read tables and convert PDF into csv file
-dfs = tabula.convert_into(file, f"{file[:-4]}.csv", output_format="csv", pages='all')
+tabula.convert_into(file, f"{file[:-4]}.csv", output_format="csv", pages='all')
 
 ## To convert all files in a folder, use this line
 #tabula.convert_into_by_batch("input_directory", output_format='csv', pages='all')
